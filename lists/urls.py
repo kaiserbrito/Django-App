93d4da2916e2ea1452b4e7e4324727d386518897
Victor Guerra <victorbcguerra@gmail.com>
2017-11-13 11:50:57 -0200

    fixing forms.py , creating the list view

--- lists/urls.py
+++ lists/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.Home.as_view()),
-    #url(r'^list/$', views.list, name='list'),
+    url(r'^list/$', views.user_list, name='user_list'),
     #url(r'^add/$', views.list, name='add'),
 ]
--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import View
+from . import views
+from .forms import UserForm
+from .models import User
 
 class Home(View):
     def get(self, request, *args):
         return render(request, 'home.html')
+
+
+def user_list(request):
+    users = User.objects.all()
+    return render(request, 'list.html', {'users': users})
